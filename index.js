d8a72d9a422ffc0e2d95eb0c22e984aca6db49cb
coopsie <aislingkcooper@hotmail.com>
2022-12-02 16:03:08 +0000

    update

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const fruitArr = ["apple", "pear"];
 
 const printFruit = () => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "apple") {
+            console.log(`I like ${arr[i]}s`);
+        }
+        else if (arr[i] === "pear")
+{
+    console.log(`I don't like ${arr[i]}s`);
+}    
+}
 
 };
 
-printFruit();
+printFruit(fruitArr);
