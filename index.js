a0fa93f8691960041f9e8a33c332f9bdad5a8e04
coopsie <aislingkcooper@hotmail.com>
2022-12-02 16:11:13 +0000

    add loop

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const fruitArr = ["apple", "pear"];
 
-const printFruit = () => {
+const printFruit = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === "apple") {
-            console.log(`I like ${arr[i]}s`);
-        }
-        else if (arr[i] === "pear")
-{
-    console.log(`I don't like ${arr[i]}s`);
-}    
-}
+        console.log(`I like ${arr[i]}s`);
+        } else if (arr[i] === "pear") {
+        console.log(`I don't like ${arr[i]}s`);
+        }    
+    }
 
 };
 
